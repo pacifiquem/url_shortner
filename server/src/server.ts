f773b0dd720d6dc5a2d4c5484e8fbb0b502ec153
pacifiquem <pacifiquemurangwa001@gmail.com>
2022-09-24 16:44:52 +0200

    dbsetup

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,12 @@
 import express, { Application } from 'express';
+import * as dotenv from 'dotenv';
+import dbConfig from './config/db.config';
+
+dotenv.config({
+    path: `${__dirname}/config/.env`
+});
 
 const app:Application = express();
+dbConfig();
 
-app.listen(2000, () => console.log('server is in Dev Mode'));
+app.listen(process.env.PORT, () => console.log(`server is in ${process.env.NODE_ENV} Mode at ${process.env.PORT}`));
