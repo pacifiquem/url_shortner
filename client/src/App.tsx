09a5814bd36a8d0d3c2e6e33608281b357c43779
pacifiquem <pacifiquemurangwa001@gmail.com>
2022-09-24 21:12:33 +0200

    modified App.ts

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import NavComponent from './component/nav';
+import BodyComponent from './component/body';
+import FooterComponent from './component/footer';
 
 function App() {
   return (
     <div className="App">
+      <NavComponent />
+      <BodyComponent />
+      <FooterComponent />
     </div>
   );
 }
