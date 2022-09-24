8f2cd618670b97074206afaf08ed6b986025ae15
pacifiquem <pacifiquemurangwa001@gmail.com>
2022-09-24 19:03:04 +0200

    modified server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,8 @@
 import express, { Application } from 'express';
 import * as dotenv from 'dotenv';
+import mongoSanitize from 'express-mongo-sanitize';
 import dbConfig from './config/db.config';
+import router from './routers/url.routers';
 
 dotenv.config({
     path: `${__dirname}/config/.env`
@@ -8,5 +10,9 @@ dotenv.config({
 
 const app:Application = express();
 dbConfig();
+
+app.use(express.json());
+app.use(mongoSanitize());
+app.use('/', router);
 
 app.listen(process.env.PORT, () => console.log(`server is in ${process.env.NODE_ENV} Mode at ${process.env.PORT}`));
