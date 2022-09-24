3f74e34c63b8f41381b35178c6ef3c038a87858f
pacifiquem <pacifiquemurangwa001@gmail.com>
2022-09-24 19:00:22 +0200

    finished postRoute

--- server/src/routers/url.routers.ts
+++ server/src/routers/url.routers.ts
@@ -1,1 +1,9 @@
-//initial commit
+import express from 'express'
+import { postUrl } from '../controllers/url.controller';
+import { Schema, JoiValidation } from '../middleware/validation';
+
+const router = express.Router();
+
+router.route('/addUrl').post(JoiValidation(Schema.data), postUrl);
+
+export default router;
