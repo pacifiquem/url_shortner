891a49bf39cd3d3df8d828ffcf4e18964bf84855
pacifiquem <pacifiquemurangwa001@gmail.com>
2022-09-24 19:01:06 +0200

    validation middleware added

---
+++ server/src/middlewares/validation.ts
@@ -0,0 +1,27 @@
+import Joi, { ObjectSchema } from "joi";
+import { Request, Response, NextFunction } from "express";
+
+
+export const JoiValidation = (schema: ObjectSchema) => {
+    return async (req:Request, res:Response, next:NextFunction) => {
+        try {
+            await schema.validateAsync(req.body);
+            next();
+        } catch (error) {
+            console.error(error);
+            res.status(422).json({
+                success: false, 
+                message: 'invalid input'
+            });
+        }
+    }
+}
+
+
+export const Schema = {
+
+    data: Joi.object ({
+        url: Joi.string().required().pattern(new RegExp('^(?:(?:http(?:s)?|ftp)://)(?:\\S+(?::(?:\\S)*)?@)?(?:(?:[a-z0-9\u00a1-\uffff](?:-)*)*(?:[a-z0-9\u00a1-\uffff])+)(?:\\.(?:[a-z0-9\u00a1-\uffff](?:-)*)*(?:[a-z0-9\u00a1-\uffff])+)*(?:\\.(?:[a-z0-9\u00a1-\uffff]){2,})(?::(?:\\d){2,5})?(?:/(?:\\S)*)?$')),
+    })
+    
+}
