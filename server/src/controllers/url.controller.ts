db8068de6352f4080b6df0a5b50a38b424c970b5
pacifiquem <pacifiquemurangwa001@gmail.com>
2022-09-24 18:59:40 +0200

    finished postCotroller

--- server/src/controllers/url.controller.ts
+++ server/src/controllers/url.controller.ts
@@ -1,1 +1,27 @@
-//initial commit
+import mongoose from "mongoose";
+import { Request, Response, NextFunction } from "express";
+import UrlModel from "../models/url.model";
+
+
+export const postUrl =async (req:Request, res:Response, next:NextFunction) => {
+
+    const { url } = req.body;
+
+    const NewUrl = await UrlModel.create({
+        url: url
+    }, (err, data) => {
+        if(err) {
+            console.log(err)
+            res.status(404).json({
+                success: false,
+                message: 'unable to insert into database'
+            });
+        }else {
+            res.status(201).json( {
+                success: true,
+                data: data
+            });
+        }
+    });
+
+}
