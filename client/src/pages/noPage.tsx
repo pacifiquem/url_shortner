94b5d2785fd6c7d8ac32d41d10facec9b2bdff58
pacifiquem <pacifiquemurangwa001@gmail.com>
2022-09-25 16:04:59 +0200

    modified pageNOtFound page

--- client/src/pages/noPage.tsx
+++ client/src/pages/noPage.tsx
@@ -1,9 +1,10 @@
 import React from "react";
+import NotFound from "../component/notfound";
 
 const NoPage:React.FC = () => {
     return (
         <>
-            
+            <NotFound />
         </>
     );
 }
