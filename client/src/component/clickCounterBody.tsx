59e80b5ff2804ef93906ecdb28fd4f1b3004849c
pacifiquem <pacifiquemurangwa001@gmail.com>
2022-09-25 14:08:01 +0200

    clickCounterBody.tsx added

---
+++ client/src/component/clickCounterBody.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import '../assets/styles/style.css';
+
+const ClickCounterBody:React.FC = () => {
+    return (
+        <>
+            <div className="clickCounterContainer">
+                <div className='clickCounterHeader'>
+                <   h1>URL Click Counter</h1>
+                    <p>Enter the URL to find out how many clicks it has received so far.</p>
+                </div>
+                <div className='clickCounterBody'>
+                    <div className="input" id='clickCounterInput'>
+                        <input type="text" placeholder="Enter the link here" />
+                        <button type="submit">Shorten URL</button>
+                    </div>
+                    <p>Example: shorturl.at/AbCdE</p>
+                </div>
+                <p>* Track the total hits of the shortened URL in real time, you do not have to register.</p>
+            </div>
+        </>
+    );
+}
+
+export default ClickCounterBody;
