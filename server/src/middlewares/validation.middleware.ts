b2c8eb9b4d11757a14539116bc1d9063589fed81
pacifiquem <pacifiquemurangwa001@gmail.com>
2022-09-26 08:55:03 +0200

    renamed file and add genUrlvalidation

---
+++ server/src/middlewares/validation.middleware.ts
@@ -0,0 +1,28 @@
+import Joi, { ObjectSchema, ref } from "joi";
+import { Request, Response, NextFunction } from "express";
+
+
+export const JoiValidation = (schema: ObjectSchema) => {
+    return async (req:Request, res:Response, next:NextFunction) => {
+        try {
+            await schema.validateAsync(req.body);
+            next();
+        } catch (error) {
+            console.error(error);
+            res.status(422).json({
+                success: false, 
+                message: 'invalid input'
+            });
+        }
+    }
+}
+
+
+export const Schema = {
+
+    data: Joi.object ({
+        url: Joi.string().required().pattern(new RegExp('^(?:(?:http(?:s)?|ftp)://)(?:\\S+(?::(?:\\S)*)?@)?(?:(?:[a-z0-9\u00a1-\uffff](?:-)*)*(?:[a-z0-9\u00a1-\uffff])+)(?:\\.(?:[a-z0-9\u00a1-\uffff](?:-)*)*(?:[a-z0-9\u00a1-\uffff])+)*(?:\\.(?:[a-z0-9\u00a1-\uffff]){2,})(?::(?:\\d){2,5})?(?:/(?:\\S)*)?$')),
+        genUrl: Joi.string().required().pattern(new RegExp('^(?:(?:http(?:s)?|ftp)://)(?:\\S+(?::(?:\\S)*)?@)?(?:(?:[a-z0-9\u00a1-\uffff](?:-)*)*(?:[a-z0-9\u00a1-\uffff])+)(?:\\.(?:[a-z0-9\u00a1-\uffff](?:-)*)*(?:[a-z0-9\u00a1-\uffff])+)*(?:\\.(?:[a-z0-9\u00a1-\uffff]){2,})(?::(?:\\d){2,5})?(?:/(?:\\S)*)?$'))
+    })
+    
+}
