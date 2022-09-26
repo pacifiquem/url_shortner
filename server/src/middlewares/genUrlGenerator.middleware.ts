e9755100cec2d5996e088e1bb0991ccb6e1cd2df
pacifiquem <pacifiquemurangwa001@gmail.com>
2022-09-26 08:56:24 +0200

    genUrl middleware adde

---
+++ server/src/middlewares/genUrlGenerator.middleware.ts
@@ -0,0 +1,25 @@
+const { v4: uuidv4 } = require('uuid');
+
+import { Response, Request, NextFunction } from 'express';
+
+const genUrlGenerator = (req:Request, res:Response, next:NextFunction):void => {
+
+    function generateUrl () {
+        const randomString:string = uuidv4();
+        const arrayOfRandomParts:string[] = randomString.split('-');
+        const randomNumber:any = Math.floor(Math.random() * 6);
+        const urlPart:string = arrayOfRandomParts[randomNumber];
+
+        return urlPart;
+    }
+
+    const generatedUrl = generateUrl();
+
+    if(generatedUrl != undefined && generatedUrl != null) {
+        req.body.genUrl = `https://atshorturl.vercel.app/${generatedUrl}`;
+        next();
+    }
+
+}
+
+export default genUrlGenerator;
