e965262e26be9eda2965b2ef1a86deef68e17a3e
pacifiquem <pacifiquemurangwa001@gmail.com>
2022-09-26 14:24:24 +0200

    bodyParser added

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,6 +4,7 @@ import mongoSanitize from 'express-mongo-sanitize';
 import dbConfig from './config/db.config';
 import router from './routers/url.routers';
 import helmet from 'helmet';
+import bodyParser from 'body-parser';
 
 dotenv.config({
     path: `${__dirname}/config/.env`
@@ -12,8 +13,8 @@ dotenv.config({
 const app:Application = express();
 app.use(helmet());
 dbConfig();
-
-app.use(express.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 app.use(mongoSanitize());
 app.use('/', router);
 
