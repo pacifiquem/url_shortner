38666f0c506cd43cac5eaf2c8d7c886eca973db2
pacifiquem <pacifiquemurangwa001@gmail.com>
2022-09-26 20:23:29 +0200

    home route added

--- server/src/routers/url.routers.ts
+++ server/src/routers/url.routers.ts
@@ -1,12 +1,13 @@
 import express from 'express'
-import { postUrl, ClickCounter , contactUs} from '../controllers/url.controller';
+import { postUrl, ClickCounter , contactUs, home} from '../controllers/url.controller';
 import { Schema, JoiValidation } from '../middlewares/validation.middleware';
 import genUrlGenerator from '../middlewares/genUrlGenerator.middleware';
 import bodyParser from 'body-parser';
 
 const router = express.Router();
 
-router.route('/').post(bodyParser.json(), genUrlGenerator, JoiValidation(Schema.data) ,postUrl);
+router.route('/').get(home);
+router.route('/addurl').post(bodyParser.json(), genUrlGenerator, JoiValidation(Schema.data) ,postUrl);
 router.route('/clickcounter').get(bodyParser.json(), ClickCounter);
 router.route('/contactus').post(contactUs);
 
