9dc575bf68142a65b57f33168e379c3dd28aea39
pacifiquem <pacifiquemurangwa001@gmail.com>
2022-09-26 15:51:33 +0200

    contact.model.ts added

---
+++ server/src/models/contact.model.ts
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const ContactSchema:mongoose.Schema = new mongoose.Schema({
+
+    email: {
+        type:String, 
+        required: true
+    },
+
+    name: {
+        type:String,
+        required: true
+    },
+
+    message: {
+        type: String,
+        required: true
+
+    },
+
+    sentAt: {
+        type: Date,
+        default: new Date()
+    }
+
+});
+
+const ContactModel = mongoose.model('Contact', ContactSchema);
+
+export default ContactModel;
