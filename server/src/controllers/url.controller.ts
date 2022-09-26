f35cb3d1ca923524ec587e9dd759f323b2585f60
pacifiquem <pacifiquemurangwa001@gmail.com>
2022-09-26 08:53:26 +0200

    add trycatch ClickCounter controller

--- server/src/controllers/url.controller.ts
+++ server/src/controllers/url.controller.ts
@@ -5,10 +5,12 @@ import UrlModel from "../models/url.model";
 
 export const postUrl =async (req:Request, res:Response, next:NextFunction) => {
 
-    const { url } = req.body;
+try {
 
+    const { url, genUrl } = req.body;
     const NewUrl = await UrlModel.create({
-        url: url
+        url: url,
+        genUrl: genUrl
     }, (err, data) => {
         if(err) {
             console.log(err)
@@ -23,5 +25,48 @@ export const postUrl =async (req:Request, res:Response, next:NextFunction) => {
             });
         }
     });
+
+} catch (error) {
+
+    console.log(error)
+    res.status(500).json({
+        success: 'false',
+        message: 'server have failed'
+    });
+
+}
+
+}
+
+
+export const ClickCounter = async(req:Request, res:Response, next:NextFunction) => {
+    try {
+
+        const providedUrl  = req.body;
+        const url = await UrlModel.findOne({
+            genUrl: providedUrl
+        });
+
+        if(url) {
+            res.status(200).json({
+                success: 'true',
+                data: url.visitCount
+            });
+        }else {
+            res.status(404).json({
+                success: 'false',
+                message: 'url not found'
+            });
+        }
+
+    } catch (error) {
+
+        console.log(error)
+        res.status(500).json({
+            success: 'false',
+            message: 'server have failed'
+        });
+
+    }
 
 }
