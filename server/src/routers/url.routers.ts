4ebe252396b6fec5b18273fc978c8f9d10595961
pacifiquem <pacifiquemurangwa001@gmail.com>
2022-09-26 14:23:42 +0200

    clickcounter router added

--- server/src/routers/url.routers.ts
+++ server/src/routers/url.routers.ts
@@ -1,10 +1,12 @@
 import express from 'express'
-import { postUrl } from '../controllers/url.controller';
+import { postUrl, ClickCounter } from '../controllers/url.controller';
 import { Schema, JoiValidation } from '../middlewares/validation.middleware';
 import genUrlGenerator from '../middlewares/genUrlGenerator.middleware';
+import bodyParser from 'body-parser';
 
 const router = express.Router();
 
-router.route('/addUrl').post(genUrlGenerator, JoiValidation(Schema.data) ,postUrl);
+router.route('/').post(bodyParser.json(), genUrlGenerator, JoiValidation(Schema.data) ,postUrl);
+router.route('/clickcounter').get(bodyParser.json(), ClickCounter);
 
 export default router;
