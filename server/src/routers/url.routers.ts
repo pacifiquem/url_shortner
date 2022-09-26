3fba43436dc02649a33ae7e4f64e8f8dd0e1ff79
pacifiquem <pacifiquemurangwa001@gmail.com>
2022-09-26 15:51:52 +0200

    contactus route added

--- server/src/routers/url.routers.ts
+++ server/src/routers/url.routers.ts
@@ -1,5 +1,5 @@
 import express from 'express'
-import { postUrl, ClickCounter } from '../controllers/url.controller';
+import { postUrl, ClickCounter , contactUs} from '../controllers/url.controller';
 import { Schema, JoiValidation } from '../middlewares/validation.middleware';
 import genUrlGenerator from '../middlewares/genUrlGenerator.middleware';
 import bodyParser from 'body-parser';
@@ -8,5 +8,6 @@ const router = express.Router();
 
 router.route('/').post(bodyParser.json(), genUrlGenerator, JoiValidation(Schema.data) ,postUrl);
 router.route('/clickcounter').get(bodyParser.json(), ClickCounter);
+router.route('/contactus').post(contactUs);
 
 export default router;
