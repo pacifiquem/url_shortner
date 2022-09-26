74c68ba12211669345b60ad9d54e27f9a5296d03
pacifiquem <pacifiquemurangwa001@gmail.com>
2022-09-26 08:57:35 +0200

    genUrl middleware added

--- server/src/routers/url.routers.ts
+++ server/src/routers/url.routers.ts
@@ -1,9 +1,10 @@
 import express from 'express'
 import { postUrl } from '../controllers/url.controller';
-import { Schema, JoiValidation } from '../middlewares/validation';
+import { Schema, JoiValidation } from '../middlewares/validation.middleware';
+import genUrlGenerator from '../middlewares/genUrlGenerator.middleware';
 
 const router = express.Router();
 
-router.route('/addUrl').post(JoiValidation(Schema.data), postUrl);
+router.route('/addUrl').post(genUrlGenerator, JoiValidation(Schema.data) ,postUrl);
 
 export default router;
