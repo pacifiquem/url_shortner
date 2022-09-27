248a8d1586034fdb1604e4f5c7e0a2af045207f9
pacifiquem <pacifiquemurangwa001@gmail.com>
2022-09-27 11:21:12 +0200

    intergrated to bcknd

--- client/src/component/homeBody.tsx
+++ client/src/component/homeBody.tsx
@@ -1,25 +1,63 @@
-import React from "react";
+import React, {useState, useRef} from "react";
 import '../assets/styles/style.css';
+import axios from 'axios';
 import UrlShower from "./homeUrlShower";
-import Spinner from 'react-bootstrap/Spinner';
+import GrowSpinner from "./Spinner";
 
 
 const BodyComponent:React.FC = () => {
+    const [inputValue, setInputValue]:[string, React.Dispatch<React.SetStateAction<string>>] = useState('');
+    const [shortUrl, setShortUrl]:[string, React.Dispatch<React.SetStateAction<string>>] = useState('');
+    const [showBody, setshowBody]:[{display: string;},React.Dispatch<React.SetStateAction<{display: string;}>>] = useState({display: 'block'});
+    const [showSpinner, setShowSpinner]:[boolean, React.Dispatch<React.SetStateAction<boolean>>] = useState(false);
+
+    const submitUrl = async() => {
+
+        setShowSpinner(!showSpinner);
+        setshowBody({display: 'none'});
+        console.log(showSpinner);
+
+        const data:void = await axios.post('https://atshorturl.herokuapp.com//addurl', {
+            url: inputValue
+        }).then((response) => {
+            console.log(response);
+            setShortUrl(`${response.data.data.genUrl}`);
+        }).catch((error) => console.log(error));
+    }
+
     return(
     <>
         <div className="body">
-            <div className="headers">
-                <h1>Paste the URL to be shortened</h1>
+            <div style={showBody}>
+                <div className="headers">
+                    <h1>Paste the URL to be shortened</h1>
+                </div>
+                <div className="input">
+                    <input type="text" placeholder="Enter the link here" value={inputValue} onChange={(e) => {setInputValue(e.target.value)}}/>
+                    <button type="submit" onClick={submitUrl}>Shorten URL</button>
+                    </div>
+                </div>
+                {(shortUrl != '' && showBody.display != 'block') ?
+                (
+                    <div>
+                        <UrlShower url={shortUrl} />
+                        <div className="footer-contents">
+                            <p>ShortURL.at is a free tool to shorten a URL or reduce a link</p>
+                            <p>Use our URL Shortener to create a shortened link making it easy to remember</p>
+                        </div>
+                    </div>
+                ):
+                (
+                    <div>
+                        {showSpinner && <GrowSpinner />}
+                        <div className="footer-contents" style={showBody}>
+                            <p>ShortURL.at is a free tool to shorten a URL or reduce a link</p>
+                            <p>Use our URL Shortener to create a shortened link making it easy to remember</p>
+                        </div>
+                    </div>
+                )
+                }
             </div>
-            <div className="input">
-                <input type="text" placeholder="Enter the link here" />
-                <button type="submit">Shorten URL</button>
-            </div>
-            <div className="footer-content">
-                <p>ShortURL.at is a free tool to shorten a URL or reduce a link</p>
-                <p>Use our URL Shortener to create a shortened link making it easy to remember</p>
-            </div>
-        </div>
         <div className="discription">
             <h2>Simple and fast URL shortener!</h2>
             <div className="paragraphs">
