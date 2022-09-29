73288101da848e28c6872145fd3c41060ea34e00
pacifiquem <pacifiquemurangwa001@gmail.com>
2022-09-29 11:11:21 +0200

    modified validations.middleware.ts

--- server/src/middlewares/validation.middleware.ts
+++ server/src/middlewares/validation.middleware.ts
@@ -22,7 +22,7 @@ export const Schema = {
 
     data: Joi.object ({
         url: Joi.string().required().pattern(new RegExp('^(?:(?:http(?:s)?|ftp)://)(?:\\S+(?::(?:\\S)*)?@)?(?:(?:[a-z0-9\u00a1-\uffff](?:-)*)*(?:[a-z0-9\u00a1-\uffff])+)(?:\\.(?:[a-z0-9\u00a1-\uffff](?:-)*)*(?:[a-z0-9\u00a1-\uffff])+)*(?:\\.(?:[a-z0-9\u00a1-\uffff]){2,})(?::(?:\\d){2,5})?(?:/(?:\\S)*)?$')),
-        genUrl: Joi.string().required().pattern(new RegExp('^(?:(?:http(?:s)?|ftp)://)(?:\\S+(?::(?:\\S)*)?@)?(?:(?:[a-z0-9\u00a1-\uffff](?:-)*)*(?:[a-z0-9\u00a1-\uffff])+)(?:\\.(?:[a-z0-9\u00a1-\uffff](?:-)*)*(?:[a-z0-9\u00a1-\uffff])+)*(?:\\.(?:[a-z0-9\u00a1-\uffff]){2,})(?::(?:\\d){2,5})?(?:/(?:\\S)*)?$'))
+        genUrl: Joi.string().required()
     })
     
 }
