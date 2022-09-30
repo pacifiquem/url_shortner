25f816a6070e7d0a66d141b31b8824202859ba57
pacifiquem <pacifiquemurangwa001@gmail.com>
2022-09-30 21:36:19 +0200

    modified url validations

--- server/src/middlewares/validation.middleware.ts
+++ server/src/middlewares/validation.middleware.ts
@@ -21,8 +21,8 @@ export const JoiValidation = (schema: ObjectSchema) => {
 export const Schema = {
 
     data: Joi.object ({
-        url: Joi.string().required().pattern(new RegExp('^(?:(?:http(?:s)?|ftp)://)(?:\\S+(?::(?:\\S)*)?@)?(?:(?:[a-z0-9\u00a1-\uffff](?:-)*)*(?:[a-z0-9\u00a1-\uffff])+)(?:\\.(?:[a-z0-9\u00a1-\uffff](?:-)*)*(?:[a-z0-9\u00a1-\uffff])+)*(?:\\.(?:[a-z0-9\u00a1-\uffff]){2,})(?::(?:\\d){2,5})?(?:/(?:\\S)*)?$')),
-        genUrl: Joi.string().required()
+        url: Joi.string().required().pattern(new RegExp('^(?:https?://|s?ftps?://)?(?!www | www\.)[A-Za-z0-9_-]+\.+[A-Za-z0-9.\/%&=\?_:;-]+$')),
+        genUrl: Joi.string().required().pattern(new RegExp('^(?:https?://|s?ftps?://)?(?!www | www\.)[A-Za-z0-9_-]+\.+[A-Za-z0-9.\/%&=\?_:;-]+$'))
     })
     
 }
