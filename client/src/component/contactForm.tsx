c834cdfe7caa0ed2c148e9fe4789bbfae2825f55
pacifiquem <pacifiquemurangwa001@gmail.com>
2022-09-30 20:17:35 +0200

    prefinal

--- client/src/component/contactForm.tsx
+++ client/src/component/contactForm.tsx
@@ -1,29 +1,67 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useState } from 'react';
 import '../assets/styles/style.css';
 
 
 
 const ContactForm:React.FC = () => {
+
+    const [email, setEmail] = useState('');
+    const [name, setName] = useState('');
+    const [message, setMessage] = useState('');
+    const [submited, setSubitted] = useState({
+        display: 'block'
+    });
+
+    const [para, setpara] = useState({
+        display: 'none'
+    });
+
+    const formHandler = async (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+
+        const response = await axios.post('https://atshorturl.herokuapp.com/contactus', {
+            name, 
+            email, 
+            message
+        }).then((res) => {
+            setSubitted({
+                display: 'none'
+            });
+
+            setpara({
+                display: 'block'
+            });
+
+        }).catch((res) => {
+            console.log(res);
+        })
+
+    }
+
     return (
         <>
+        <div style={submited}>
             <h2 className='contact'>Contact our Team</h2>
             <div className="formcontainer">
-                <form action="">
+                <form onSubmit = {formHandler}>
                     <div className="input-label">
                         <label htmlFor="Name">Name</label>
-                        <input type="text" />
+                        <input type="text" required value={name} onChange={(e) => setName(e.target.value)} />
                     </div>
                     <div className="input-label">
                         <label htmlFor="Email">E-mail</label>
-                        <input type="text" />
+                        <input type="email" required value={email} onChange={(e) => setEmail(e.target.value)} />
                     </div>
                     <div className="input-label">
                         <label htmlFor="Message">Message</label>
-                        <textarea name="message"></textarea>
+                        <textarea name="message" required value={message} onChange={(e) => setMessage(e.target.value)}></textarea>
                     </div>
                     <button type="submit">Send</button>
-                </form>
-            </div>
+                    </form>
+                </div>
+        </div>
+        <p id='feedback' style={para}>Thank you for your feedback</p>
         </>
     )
 }
